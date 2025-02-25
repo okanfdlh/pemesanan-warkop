0c0b5dc96016417c9afdb6a9e8b03fcc30fa376a
okanfdlh <ignadeafiranti@gmail.com>
2025-02-25 18:54:15 +0700

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -32,7 +32,7 @@
             <div x-data="searchComponent()" class="relative">
                 <!-- Ikon Pencarian -->
                 <a @click="showSearch = !showSearch" class="cursor-pointer">
-                    <svg class="w-6 h-6 sm:w-7 sm:h-7 text-gray-700 hover:text-green-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <svg class="w-6 h-6 sm:w-7 sm:h-7 text-gray-700 fill-current hover:text-green-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <circle fill="none" cx="12" cy="7" r="3" />
                         <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10
                             c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
@@ -66,30 +66,30 @@
             </div>
            {{-- Keranjang --}}
 
-           <a class="pl-3 inline-block no-underline hover:text-green-800 relative" href="/chart"
-   x-data="{ cartCount: localStorage.getItem('cartCount') || {{ session('cart') ? count(session('cart')) : 0 }} }"
-   x-init="
-       window.addEventListener('cart-updated', event => { 
-           cartCount = event.detail.count;
-           localStorage.setItem('cartCount', cartCount); 
-       });
-   ">
+            <a class="pl-3 inline-block no-underline hover:text-green-800 relative" href="/chart"
+            x-data="{ cartCount: localStorage.getItem('cartCount') || {{ session('cart') ? count(session('cart')) : 0 }} }"
+            x-init="
+                window.addEventListener('cart-updated', event => { 
+                    cartCount = event.detail.count;
+                    localStorage.setItem('cartCount', cartCount); 
+                });
+            ">
 
-    <svg class="fill-current hover:text-green-800" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <path d="M21,7H7.462L5.91,3.586C5.748,3.229,5.392,3,5,3H2v2h2.356L9.09,15.414C9.252,15.771,9.608,16,10,16h8 
-                c0.4,0,0.762-0.238,0.919-0.606l3-7c0.133-0.309,0.101-0.663-0.084-0.944C21.649,7.169,21.336,7,21,7z 
-                M17.341,14h-6.697L8.371,9 h11.112L17.341,14z" />
-        <circle cx="10.5" cy="18.5" r="1.5" />
-        <circle cx="17.5" cy="18.5" r="1.5" />
-    </svg>
+                <svg class="fill-current hover:text-green-800 w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M21,7H7.462L5.91,3.586C5.748,3.229,5.392,3,5,3H2v2h2.356L9.09,15.414C9.252,15.771,9.608,16,10,16h8 
+                            c0.4,0,0.762-0.238,0.919-0.606l3-7c0.133-0.309,0.101-0.663-0.084-0.944C21.649,7.169,21.336,7,21,7z 
+                            M17.341,14h-6.697L8.371,9 h11.112L17.341,14z" />
+                    <circle cx="10.5" cy="18.5" r="1.5" />
+                    <circle cx="17.5" cy="18.5" r="1.5" />
+                </svg>
 
-    <!-- Badge jumlah pesanan -->
-    <span x-show="cartCount > 0" 
-          x-text="cartCount"
-          class="absolute -top-1 bg-red-600 text-white text-xs font-bold px-2 py-1 rounded-full 
-                 transform translate-x-1/2 -translate-y-1/2">
-    </span>
-</a>
+                <!-- Badge jumlah pesanan -->
+                <span x-show="cartCount > 0" 
+                    x-text="cartCount"
+                    class="absolute -top-1 bg-red-600 text-white text-xs font-bold px-2 py-1 rounded-full 
+                            transform translate-x-1/2 -translate-y-1/2">
+                </span>
+            </a>
 
     </div>
 </div>
